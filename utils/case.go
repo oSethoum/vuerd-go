aa461d60338b46098789823c4c26f03617667cb2
oSethoum@gmail.com <os.sethoum@gmail.com>
2022-03-12 17:13:06 +0100

    update utils

--- utils/case.go
+++ utils/case.go
@@ -9,16 +9,3 @@ func CorrectCase(s string) string {
 	}
 	return s
 }
-
-func SnakeToCamel(word string) string {
-	words := strings.Split(word, "_")
-	words[0] = strings.ToLower(words[0])
-	for i := 1; i < len(words); i++ {
-		words[i] = strings.ToUpper(string(words[i][0])) + strings.TrimPrefix(words[i], string(words[i][0]))
-	}
-	return strings.Join(words, "")
-}
-
-func SnakeToPascal(word string) string {
-	return "hello"
-}
--- utils/json.go
+++ utils/json.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"os"
+	"vuerd/types"
 )
 
 func ReadJSON(v interface{}, path string) error {
@@ -16,10 +17,10 @@ func ReadJSON(v interface{}, path string) error {
 }
 
 func WriteJSON(v interface{}, path string) error {
-	buffer, err := os.ReadFile(path)
+	buffer, err := json.Marshal(v)
 	if err != nil {
 		return err
 	}
-	json.Unmarshal(buffer, v)
-	return nil
+
+	return WriteFile(types.File{Path: path, Buffer: string(buffer)})
 }
